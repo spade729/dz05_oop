f8c83668385bf0fb32819380b8465a14b3002661
spade729 <i-v-babin@yandex.ru>
2025-10-06 10:31:32 +0300

    v1.02

--- src/dz05_oop/ex01.py
+++ src/dz05_oop/ex01.py
@@ -26,6 +26,16 @@ grad_student.add_publication()
 grad_student.add_publication()
 # Проверка количества публикаций должна быть через специальный метод
 
+3. Создайте систему управления студентами:
+
++ Реализуйте класс <b>StudentManager</b> для работы с коллекцией студентов;
++ Добавьте магические методы в классы:
+
+    + <b>\_\_str\_\_</b> и <b>\_\_repr\_\_</b> для красивого вывода;
+    + <b>\_\_len\_\_</b> для StudentManager (возвращает количество студентов);
+    + <b>\_\_getitem\_\_</b> для доступа к студентам по индексу;
+
++ Реализуйте полиморфизм - метод, который работает с любыми типами студентов.
 
 # Образец использования:
 manager = StudentManager()
@@ -82,4 +92,43 @@ grad_student.add_publication()
 grad_student.add_publication()
 print(grad_student.publications) 
 
+class StudentManager:
+    def __init__(self):
+        self.students = []
+
+    def add_student(self, obj:Student):
+        self.students.append(obj)
+
+    def print_all_info(self):
+        for obj in self.students:
+            print(obj.get_info())
+
+    def __str__(self):
+        return f'StudentManager, {len(self.students)} студентов'
+
+    def __len__(self):
+        return len(self.students)
+
+    def __getitem__(self, i):
+        return self.students[i]
+    
+
+
 print('3)')
+# Образец использования:
+manager = StudentManager()
+
+student11 = Student("Анна", 20)
+student2 = GraduateStudent("Петр", 25, "AI")
+
+manager.add_student(student11)
+manager.add_student(student2)
+
+print(str(manager)) 
+print(len(manager))  # Должно вывести: 2
+print(manager[0])    # Должен вывести первого студента
+print(manager[0].get_info())
+
+# Полиморфный метод, который выводит информацию о всех студентах
+manager.print_all_info()
+
